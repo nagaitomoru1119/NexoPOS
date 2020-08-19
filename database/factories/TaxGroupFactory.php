02339b81c1a233c1f8b258411df964a8f1acb7a9
Developper <carlosjohnsonluv2004@gmail.com>
2020-08-19 15:56:06 +0100

    WIP - Added : new seeders - Added : new notifications menus - Added : product & variations (incomplete) - Updated : Tailwind to version 1.7.1

--- database/factories/TaxGroupFactory.php
+++ database/factories/TaxGroupFactory.php
@@ -3,14 +3,14 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model;
-use App\Models\UnitGroup;
+use App\Models\TaxGroup;
 use App\Models\User;
 use Faker\Generator as Faker;
 
-$factory->define( UnitGroup::class, function (Faker $faker) {
+$factory->define( TaxGroup::class, function (Faker $faker) {
     return [
         'name'  =>  $faker->word,
         'description'   =>  $faker->sentence,
-        'author'        =>  $faker->randomElement( User::get()->map( fn( $user ) => $user->id ) ),
+        'author'        =>  $faker->randomElement( User::get()->map( fn( $user ) => $user->id ) ),    
     ];
 });
