70d2e69a86e5be84ba55d801f2c09b444fa80480
Nagai Tomoru <nagaitomoru1119@outlook.com>
2020-09-21 02:32:31 +0100

    Update - Added : POS (WIP) - Added : Medias + UI + Routes

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -4342,7 +4342,7 @@ eval("module.exports = function(module) {\n\tif (!module.webpackPolyfill) {\n\t\
 
 /***/ }),
 
-/***/ 4:
+/***/ 5:
 /*!***********************************************************************!*\
   !*** multi vue lodash chart.js axios moment rxjs rx vue-router dayjs ***!
   \***********************************************************************/
