330b2a312a35a2bf5b8185d8bdf8f982fb69dd7a
Nagai Tomoru <nagaitomoru1119@outlook.com>
2021-01-24 00:54:41 +0100

    Update

--- app/Models/CustomerCouponProduct.php
+++ app/Models/CustomerCouponProduct.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class CouponProduct extends NsModel
+class CustomerCouponProduct extends NsModel
 {
     use HasFactory;
     
-    protected $table    =   'nexopos_' . 'coupons_products';
+    protected $table    =   'nexopos_' . 'customers_coupons_products';
 
     public function coupon()
     {
